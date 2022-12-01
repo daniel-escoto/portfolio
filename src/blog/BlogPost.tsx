af8a70bb48903e40c98fcbbd7f29aee8fe76846e
Daniel Escoto <escotodanny62@gmail.com>
2022-11-30 22:08:31 -0800

    adjust format

--- src/blog/BlogPost.tsx
+++ src/blog/BlogPost.tsx
@@ -8,12 +8,10 @@ interface Props {
 export default function BlogPost({ post }: Props) {
   return (
     <Layout>
-      <h1 className="text-6xl font-bold text-center mt-20 dark:text-gray-200">
+      <h1 className="text-6xl font-bold mt-20 dark:text-gray-200">
         {post.title}
       </h1>
-      <p className="text-2xl text-center mt-10 dark:text-gray-200">
-        {post.content}
-      </p>
+      <p className="text-xl mt-10 dark:text-gray-200">{post.content}</p>
     </Layout>
   );
 }
