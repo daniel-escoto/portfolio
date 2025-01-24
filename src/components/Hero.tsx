734eb3859bb045a095e6074d907a7f5156746d04
Daniel Escoto <escotodanny62@gmail.com>
2025-01-24 18:59:24 -0500

    cleanup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,27 +5,27 @@ import Link from "./Link";
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center">
-      <div className="container mx-auto px-6 py-20 flex flex-col-reverse md:flex-row items-between">
+      <div className="container mx-auto  py-20 flex flex-col md:flex-row items-between gap-10">
+        <div className="flex-none flex items-center justify-center">
+          <GameOfLife />
+        </div>
         <div className="md:mr-6 dark:text-white">
-          <h2 className="text-4xl font-bold">Daniel Escoto</h2>
-          <h3 className="text-2xl mt-2 mb-8">Software Engineer</h3>
+          <h2 className="text-4xl font-bold mb-4">Daniel Escoto</h2>
           <p>
-            I am passionate about creating impactful digital solutions,
-            particularly in frontend development, from websites to mobile
-            applications. I enjoy making well-thought-out, accessible, and
-            visually appealing user interfaces that deliver seamless,
-            high-performance user experiences. Beyond coding, I enjoy exploring
-            emerging technologies, cooking, spending time with my dog, and
-            traveling. Currently, I am a Software Engineer at{" "}
+            I'm a Software Engineer focused on building early-stage startups.
+            Previously, I was a founding engineer at Techstars-backed Perform.
+            I'm currently joining Y Combinator-backed{" "}
             <a
-              href="https://www.frontdoorhome.com/"
+              href="https://www.withgarage.com/"
               className="text-blue-600 dark:text-blue-400"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Frontdoor
-            </a>
-            .
+              Garage
+            </a>{" "}
+            as a founding engineer, where we're building a marketplace for fire
+            trucks and emergency equipment. Let's connect and discuss how we can
+            build something great together.
           </p>
 
           <div className="flex flex-row space-x-4 mt-8">
@@ -49,10 +49,6 @@ export default function Hero() {
               Resume
             </Link>
           </div>
-        </div>
-
-        <div className="flex-none flex items-center justify-center">
-          <GameOfLife />
         </div>
       </div>
     </section>
